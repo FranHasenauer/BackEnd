7854c78caa1d54b5ba580ab11a9575d32d9c1525
FranHasenauer <franciscohasenauer@gmail.com>
2022-09-05 15:38:13 -0300

    primer commit

---
+++ src/main/java/com/portfolio/Fran/FranApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Fran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/Fran/FranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Fran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
