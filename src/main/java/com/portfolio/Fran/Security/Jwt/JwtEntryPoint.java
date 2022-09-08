bec4a12854af7f29482a64dab4956259f55ccddc
FranHasenauer <franciscohasenauer@gmail.com>
2022-09-08 07:19:39 -0300

    terminado el paquete JWT

---
+++ src/main/java/com/portfolio/Fran/Security/Jwt/JwtEntryPoint.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.Fran.Security.Jwt;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+/**
+ *
+ * @author franc
+ */
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+    
+    private final static Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        logger.error("Falló el metodo commence");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED); 
+        
+    }
+    
+    
+}
+
