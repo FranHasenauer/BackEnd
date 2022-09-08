fe6f20ff46c5f05388c4c1999b97892521e5429c
FranHasenauer <franciscohasenauer@gmail.com>
2022-09-08 10:04:56 -0300

    terminado el controller y solucionado bugs

--- src/main/java/com/portfolio/Fran/Security/Jwt/JwtEntryPoint.java
+++ src/main/java/com/portfolio/Fran/Security/Jwt/JwtEntryPoint.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfolio.Fran.Security.Jwt;
 
 import java.io.IOException;
@@ -12,22 +8,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
-/**
- *
- * @author franc
- */
+@Component
 public class JwtEntryPoint implements AuthenticationEntryPoint {
-    
+
     private final static Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response
+            , AuthenticationException authException) throws IOException, ServletException {
         logger.error("Falló el metodo commence");
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED); 
-        
-    }
-    
-    
-}
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 
+    }
+}
