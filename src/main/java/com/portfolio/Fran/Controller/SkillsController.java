d26d4e448088996d225aaa41caaf22a0a26d48af
FranHasenauer <franciscohasenauer@gmail.com>
2022-09-30 06:41:12 -0300

    primer commit en heroku

--- src/main/java/com/portfolio/Fran/Controller/SkillsController.java
+++ src/main/java/com/portfolio/Fran/Controller/SkillsController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("skills")
+@RequestMapping("skill")
 @CrossOrigin(origins = "http://localhost:4200")
 public class SkillsController {
 
