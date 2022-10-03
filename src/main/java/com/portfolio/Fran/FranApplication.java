41d504936491d4abb6386a66055940277b65addb
FranHasenauer <franciscohasenauer@gmail.com>
2022-10-03 17:51:15 -0300

    modifique letra de Lista para probar :(

--- src/main/java/com/portfolio/Fran/FranApplication.java
+++ src/main/java/com/portfolio/Fran/FranApplication.java
@@ -2,8 +2,10 @@ package com.portfolio.Fran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "https://frontendfranh.web.app/")
 public class FranApplication {
 
 	public static void main(String[] args) {
