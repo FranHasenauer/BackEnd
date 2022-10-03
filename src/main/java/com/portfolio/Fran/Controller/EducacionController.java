8f1fbfb7987c945064a729e3bfb6964d020f4262
FranHasenauer <franciscohasenauer@gmail.com>
2022-10-03 19:33:41 -0300

    probando crear un metodo en el main

--- src/main/java/com/portfolio/Fran/Controller/EducacionController.java
+++ src/main/java/com/portfolio/Fran/Controller/EducacionController.java
@@ -8,10 +8,8 @@ import com.portfolio.Fran.Service.EducacionService;
 import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +26,7 @@ public class EducacionController {
     @Autowired
     EducacionService eduService;
    
-    @Bean
+    
       @GetMapping("/lista")
     public ResponseEntity<List<Educacion>> list() {
         List<Educacion> list = eduService.list();
