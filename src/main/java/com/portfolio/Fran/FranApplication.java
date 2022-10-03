0537dfe6f82ae372a02881527ca7df2e07c41b78
FranHasenauer <franciscohasenauer@gmail.com>
2022-10-03 19:48:45 -0300

    a ver

--- src/main/java/com/portfolio/Fran/FranApplication.java
+++ src/main/java/com/portfolio/Fran/FranApplication.java
@@ -2,23 +2,12 @@ package com.portfolio.Fran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FranApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FranApplication.class, args);
-    }
-    @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer() {
-        public void addCorsMapping(CorsRegistry registry){
-            registry.addMapping("/lista").allowedOrigins("https://frontendfranh.web.app/");
-        }
-        };
     }
 
 }
