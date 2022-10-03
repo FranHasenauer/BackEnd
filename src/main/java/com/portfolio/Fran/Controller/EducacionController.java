77b6ae603432ddcb448e06233888f47dd93866b9
FranHasenauer <franciscohasenauer@gmail.com>
2022-10-03 16:50:22 -0300

    modifique letra de Lista para probar :(

--- src/main/java/com/portfolio/Fran/Controller/EducacionController.java
+++ src/main/java/com/portfolio/Fran/Controller/EducacionController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("edu")
+@RequestMapping("/edu")
 @CrossOrigin(origins = "https://frontendfranh.web.app/") //
 //@CrossOrigin(origins = "http://localhost:4200")
 public class EducacionController {
@@ -30,12 +30,12 @@ public class EducacionController {
     EducacionService eduService;
    
     
-      @GetMapping("edu/lista")
+      @GetMapping("/lista")
     public ResponseEntity<List<Educacion>> list() {
         List<Educacion> list = eduService.list();
         return new ResponseEntity(list, HttpStatus.OK);
     }
-     @GetMapping("edu/detail/{id}")
+     @GetMapping("/detail/{id}")
     public ResponseEntity<Educacion> getById(@PathVariable("id") int id) {
 
         if (!eduService.existById(id)) {
@@ -47,7 +47,7 @@ public class EducacionController {
         return new ResponseEntity(educacion, HttpStatus.OK);
 
     }
-     @PostMapping("edu/create")
+     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody DtoEducacion dtoEdu) {
         if (StringUtils.isBlank(dtoEdu.getNombreE())) {
             return new ResponseEntity(new Mensaje("Necesitas poner un nombre"), HttpStatus.BAD_REQUEST);
@@ -60,7 +60,7 @@ public class EducacionController {
         return new ResponseEntity(new Mensaje("Educacion creada!"), HttpStatus.OK);
     }
 
-    @PutMapping("edu/update/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoEducacion dtoEdu) {
         if (!eduService.existById(id)) {
             return new ResponseEntity(new Mensaje("ID inexistente"), HttpStatus.BAD_REQUEST);
@@ -84,7 +84,7 @@ public class EducacionController {
     }
 
     
-    @DeleteMapping("edu/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id) {
         if (!eduService.existById(id)) {
             return new ResponseEntity(new Mensaje("ID inexistente"), HttpStatus.BAD_REQUEST);
