094ca43ec646e34814fb8628bc41c0eda1d8b2cd
FranHasenauer <franciscohasenauer@gmail.com>
2022-10-03 16:49:16 -0300

    modifique letra de Lista para probar :(

--- src/main/java/com/portfolio/Fran/Controller/EducacionController.java
+++ src/main/java/com/portfolio/Fran/Controller/EducacionController.java
@@ -30,12 +30,12 @@ public class EducacionController {
     EducacionService eduService;
    
     
-      @GetMapping("/lista")
+      @GetMapping("edu/lista")
     public ResponseEntity<List<Educacion>> list() {
         List<Educacion> list = eduService.list();
         return new ResponseEntity(list, HttpStatus.OK);
     }
-     @GetMapping("/detail/{id}")
+     @GetMapping("edu/detail/{id}")
     public ResponseEntity<Educacion> getById(@PathVariable("id") int id) {
 
         if (!eduService.existById(id)) {
@@ -47,7 +47,7 @@ public class EducacionController {
         return new ResponseEntity(educacion, HttpStatus.OK);
 
     }
-     @PostMapping("/create")
+     @PostMapping("edu/create")
     public ResponseEntity<?> create(@RequestBody DtoEducacion dtoEdu) {
         if (StringUtils.isBlank(dtoEdu.getNombreE())) {
             return new ResponseEntity(new Mensaje("Necesitas poner un nombre"), HttpStatus.BAD_REQUEST);
@@ -60,7 +60,7 @@ public class EducacionController {
         return new ResponseEntity(new Mensaje("Educacion creada!"), HttpStatus.OK);
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping("edu/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoEducacion dtoEdu) {
         if (!eduService.existById(id)) {
             return new ResponseEntity(new Mensaje("ID inexistente"), HttpStatus.BAD_REQUEST);
@@ -84,7 +84,7 @@ public class EducacionController {
     }
 
     
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("edu/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id) {
         if (!eduService.existById(id)) {
             return new ResponseEntity(new Mensaje("ID inexistente"), HttpStatus.BAD_REQUEST);
