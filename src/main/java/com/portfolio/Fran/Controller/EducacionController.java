1b1a11248aeb8bec3d005d86e223710e4b681d72
FranHasenauer <franciscohasenauer@gmail.com>
2022-10-07 21:21:36 -0300

    modificaciones mainSecurity3

--- src/main/java/com/portfolio/Fran/Controller/EducacionController.java
+++ src/main/java/com/portfolio/Fran/Controller/EducacionController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("edu")
+@RequestMapping("/edu")
 public class EducacionController {
     
     @Autowired
